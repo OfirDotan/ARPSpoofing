99668870fe59de0c1f186e72f14ea63bd02d2a21
Ofir Dotan <fifodotan@gmail.com>
2025-04-26 14:13:25 +0300

    Fixed constants

--- c/arp.h
+++ c/arp.h
@@ -1,6 +1,9 @@
 #ifndef ARP_H
 #define ARP_H
 
+#define MAC_ADDRESS_SIZE 6
+#define IP_ADDRESS_SIZE 4
+
 #include <stdint.h>
 #include "ether.h"
 
@@ -8,8 +11,6 @@ const uint16_t ARP_REQUEST = 1;
 const uint16_t ARP_RESPONSE = 2;
 const uint16_t ETHERNET_CODE = 1;
 const uint16_t IPV4_CODE = 0x0800;
-const int MAC_ADDRESS_SIZE = 6;
-const int IP_ADDRESS_SIZE = 4;
 
 typedef struct arp_header {
 	uint16_t hardwareType;
@@ -21,8 +22,8 @@ typedef struct arp_header {
 	uint8_t senderHardwareAddress[MAC_ADDRESS_SIZE]; // Sender's MAC address
 	uint8_t targetHardwareAddress[MAC_ADDRESS_SIZE]; // Target's MAC address
 	
-		uint8_t senderProtocolAddress[IP_ADRESS_SIZE]; //Sender's IP address
-	uint8_t targetProtocolAddress[IP_ADRESS_SIZE]; // Target's IP address
+		uint8_t senderProtocolAddress[IP_ADDRESS_SIZE]; //Sender's IP address
+	uint8_t targetProtocolAddress[IP_ADDRESS_SIZE]; // Target's IP address
 } arp_header;
 
 
@@ -33,4 +34,4 @@ typedef struct arp_packet {
 
 int initialize_arp(arp_packet* packet, uint8_t senderHardwareAddress[], uint8_t targetHardwareAddress[], uint8_t senderProtocolAddress[], uint8_t targetProtocolAddress[], uint16_t operation);
 
-#endif ARP_H
+#endif
