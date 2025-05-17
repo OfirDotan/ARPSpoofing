88c6e65618db85f5469cfb4604610f2ce1d87609
Ofir Dotan <fifodotan@gmail.com>
2025-05-17 20:26:05 +0300

    Fixed ether frames implementation

--- c/ether.h
+++ c/ether.h
@@ -3,21 +3,24 @@
 
 #define MAC_ADDRESS_SIZE 6
 #define PAYLOAD_SIZE_LIMIT 1500
+#define MINIMUM_PAYLOAD_SIZE 46
 
 #include <stdint.h>
 
-typedef struct ether_header {
-	uint8_t destinationAddress[MAC_ADDRESS_SIZE];
-	uint8_t sourceAddress[MAC_ADDRESS_SIZE];
-	uint16_t ethernetType;
+typedef struct {
+	uint8_t destination_address[MAC_ADDRESS_SIZE];
+	uint8_t source_address[MAC_ADDRESS_SIZE];
+	uint16_t ethernet_type;
 } ether_header;
 
 
-typedef struct ethernet_frame {
-	ether_header ethernetHeader; // Ethernet header
-	uint8_t payload[PAYLOAD_SIZE_LIMIT]; // Payload
+typedef struct {
+	ether_header ethernet_header; // Ethernet header
+	uint8_t payload[PAYLOAD_SIZE_LIMIT]; // Payload data
+	uint8_t payload_size;
 } ethernet_frame;	
 
-int initialize_ethernet(ethernet_frame* frame, uint8_t destinationAddress[], uint8_t sourceAddress[], uint16_t ethernetType, uint8_t payload[]);
+int initialize_ethernet(ethernet_frame* frame, uint8_t destination_address[], size_t destination_size, uint8_t source_address[], size_t source_size, uint16_t ethernet_type, uint8_t payload[], size_t payload_size);
+int send_frame(ethernet_frame* frame);
 
-#endif ETHER_H
+#endif
