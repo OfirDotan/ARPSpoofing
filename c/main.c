ce825357a79de6280f4ec30e020e202fd0530525
Ofir Dotan <fifodotan@gmail.com>
2025-05-17 20:26:25 +0300

    Example main.c for testing

--- c/main.c
+++ c/main.c
@@ -3,8 +3,16 @@
 #include "arp.h"
 
 int main() {
-	arp_packet packet;
+	//test ether send
 
+	ethernet_frame frame;
+	uint8_t source_address[MAC_ADDRESS_SIZE] = {0x00, 0x0C, 0x29, 0xF4, 0x92, 0xA0};
+	uint8_t dest_address[MAC_ADDRESS_SIZE] = {0x00, 0x0C, 0x29, 0xF4, 0x92, 0xA0};
+    uint8_t payload[46] = {0xAB};
 
-	printf("Hello world!");
+	initialize_ethernet(&frame, dest_address, 6, source_address, 6, 0x88B5, payload, 46);
+	
+
+	int result = send_frame(&frame);
+	printf("Status: %d\n", result);
 }
