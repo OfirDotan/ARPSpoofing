ee33131339f0a165226aa08811cbf992afcc7684
Ofir Dotan <fifodotan@gmail.com>
2025-06-14 20:29:23 +0300

    Fixed ARP request

--- c/src/arp_spoofer.c
+++ c/src/arp_spoofer.c
@@ -1,5 +1,1 @@
 #include "arp_spoofer.h"
-
-void arp_poisoning(target_host_ip: str, target_host_mac: str, replace_host_ip: str, replace_host_mac: str){
-    
-}
--- c/src/main.c
+++ c/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/socket.h> // For socket functions
 #include <arpa/inet.h> // For htons
+#include <unistd.h> // For close
 
 #include "ether.h"
 #include "arp.h"
@@ -25,16 +26,24 @@ int main() {
 
 	int res = get_mac_address(
 		(uint8_t[]){0x00, 0x0C, 0x29, 0xF4, 0x92, 0xA0}, MAC_ADDRESS_SIZE,
-		(uint8_t[]){192, 168, 159, 128}, IP_ADDRESS_SIZE,
+		(uint8_t[]){10, 0, 0, 79}, IP_ADDRESS_SIZE,
 		(uint8_t[]){10, 0, 0, 16}, IP_ADDRESS_SIZE,
 		&target_hardware, raw_socket_fd
 	);
 	printf("Result: %d\n", res);
 
-	if (res >= 0) {
+	printf("Sender Hardware Address: ");
+	if(target_hardware != NULL) {
 		for(int i = 0; i < MAC_ADDRESS_SIZE; i++) {
-			printf("%d ", target_hardware[i]);
+			if(i + 1 != MAC_ADDRESS_SIZE) {
+				printf("%02X:", target_hardware[i]);
+			} else {
+				printf("%02X", target_hardware[i]);
+			}
 		}
 		printf("\n");
 	}
+
+	free(target_hardware);
+	close(raw_socket_fd);
 }
